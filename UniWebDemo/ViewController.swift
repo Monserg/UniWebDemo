57fc63ce429aa8cafb92a5ab306c49cc821de115
Sergey Monasturskiy <msm72@mail.ru>
2021-01-15 07:51:13 +0200

    Initial Commit

---
+++ UniWebDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UniWebDemo
+//
+//  Created by Sergey Monastyrskiy on 15.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
